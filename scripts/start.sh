ef7cf30a4e050430ef6524dc93f07364fb035035
Nirmal <nirmalk.web@gmail.com>
2021-05-15 13:03:43 +0530

    updated posts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /Users/garden/projects/nkdews.me 
 enwrite -n "AWritingDaily" -t published  -o /Users/garden/projects/nkdews.me/  --remove-tags "published" 
+git add .
+git commit -m "updated posts"
+git push
 /usr/local/bin/hugo serve --port 9003 -D  --watch=true
